2fcad2e9291e1a3513aaca909c1833d7f7cf859f
santicano <canosantiago404@gmail.com>
2024-03-09 10:32:14 -0300

    añadido libreria

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { BiSearch } from "react-icons/bi";
 
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
+import Library from "./Library";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -19,13 +20,13 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     () => [
       {
         icon: HiHome,
-        label: "Home",
+        label: "Inicio",
         active: pathname !== "/search",
         href: "/",
       },
       {
         icon: BiSearch,
-        label: "Search",
+        label: "Buscar",
         active: pathname === "/search",
         href: "/search",
       },
@@ -43,8 +44,11 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             ))}
           </div>
         </Box>
-        <Box className="overflow-y-auto h-full">Song Library</Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
