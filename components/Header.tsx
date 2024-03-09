46d532047432c6897625bffe985ce1fafdbb3259
santicano <canosantiago404@gmail.com>
2024-03-09 11:03:37 -0300

    añadido botones a mobile view en header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 
@@ -36,6 +38,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
           >
             <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
           </button>
         </div>
       </div>
