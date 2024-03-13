91490b3ffd5a84fd8f16a51fd2154ce249e3610c
Santi <canosantiago404@gmail.com>
2024-03-13 14:35:18 -0300

    añadidos controles en el player

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
@@ -15,7 +16,9 @@ const Player = () => {
   }
 
   return (
-    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4"></div>
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+    </div>
   );
 };
 
